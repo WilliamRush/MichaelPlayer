6944dd718841e962471920c58c3d74ba19927e6c
Michael <467906556@qq.com>
2022-05-14 17:12:20 +0800

    feat;添加配置文件

---
+++ MainWindow/MainWIndowControl.cpp
@@ -0,0 +1,36 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <iostream>
+//
+//extern "C" {
+//#include "libavformat/avformat.h"
+//#include "libavcodec/avcodec.h"
+//#include "x264.h"
+//}
+//
+//int main(int argc, char *argv[])
+//{
+//    x264_param_t param;
+//    x264_param_default(&param);
+//    auto codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+//    if (codec) {
+//        std::cout << "success!!!" << std::endl;
+//    }
+//
+//#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+//    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+//#endif
+//
+//    QGuiApplication app(argc, argv);
+//
+//    QQmlApplicationEngine engine;
+//    const QUrl url(QStringLiteral("qrc:/main.qml"));
+//    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+//                     &app, [url](QObject *obj, const QUrl &objUrl) {
+//        if (!obj && url == objUrl)
+//            QCoreApplication::exit(-1);
+//    }, Qt::QueuedConnection);
+//    engine.load(url);
+//
+//    return app.exec();
+//}
